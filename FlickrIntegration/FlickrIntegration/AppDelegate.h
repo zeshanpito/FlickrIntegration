41d87e7f2f626ede867a6b489dcff1dc7a99b4fa
Zeshan Hayder <zeshanhayder@gmail.com>
2015-06-04 15:48:53 +0500

    added base project

---
+++ FlickrIntegration/FlickrIntegration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrIntegration
+//
+//  Created by Zeshan Hayder on 04/06/2015.
+//  Copyright (c) 2015 Putitout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrIntegration/FlickrIntegration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrIntegration
+//
+//  Created by Zeshan Hayder on 04/06/2015.
+//  Copyright (c) 2015 Putitout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
